e1ce79435505a83f8c79bbe62678325d35116b06
Konstantin Sologub <kon9397@gmail.com>
2021-11-21 13:11:02 +0200

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ProductCardsComponent } from './components/product-cards/product-cards.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SliderComponent,
+    ProductsComponent,
+    ProductCardsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
